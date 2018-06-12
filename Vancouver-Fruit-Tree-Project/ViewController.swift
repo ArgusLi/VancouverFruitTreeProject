3a1e1f31f1895555daf2e98684bc148a5febd27e
Tom <Artem@d207-023-162-141.wireless.sfu.ca>
2018-06-12 10:30:03 -0700

    Initial Commit

---
+++ Vancouver-Fruit-Tree-Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vancouver-Fruit-Tree-Project
+//
+//  Created by Artem Gromov on 2018-06-12.
+//  Copyright © 2018 Harvest8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
