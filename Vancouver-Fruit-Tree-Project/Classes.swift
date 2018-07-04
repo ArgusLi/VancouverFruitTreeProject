4cb04939c1f3e6195155e98e655aea8fa2b91faa
Argus Li <argus@imac.hitronhub.home>
2018-07-04 13:04:26 -0700

    Ver 1 completed

--- Vancouver-Fruit-Tree-Project/Classes.swift
+++ Vancouver-Fruit-Tree-Project/Classes.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+let dbInterface=DatabaseInterface()
+let pickEvents=PickEvents()
 
+/*TODO Ver2
 class team:NSObject{
     var teamLeader:String?
     var teamMembers=Set<String>()
     var teamName:String?
     var pickEvent:String?
     var teamID:String?
-    var dbInterface=DatabaseInterface()
+
     
     func addMember(accountID:String){
         //Add to database
@@ -36,34 +39,51 @@ class team:NSObject{
         pickEvent=nil
     }
 }
+ */
 class coordinator:NSObject{
     var adminID:String?
-    var dbInterface=DatabaseInterface()
+    var pickEvent=pickEvents
+    var success:Int=0
     
+    /*
     func viewYield()->String{
-        //Todo
-        return "yield"
+        //Todo ver2
     }
     func pushNotificationforPick(){
-        //Todo
+        //Todo ver 2
     }
+ */
     func createPickEvent(eventTime: String, eventDate: String, latitude: NSNumber, longitude: NSNumber, teamID: String){
-        dbInterface.createPickEvents(eventTime: eventTime, eventDate: eventDate , latitude: latitude, longitude: longitude, teamID: teamID)
+        dbInterface.createPickEvents(eventTime: eventTime, eventDate: eventDate, latitude: latitude, longitude: longitude, teamID: teamID)
     }
-    func assignPickToTeam(pickID:String,teamID:String){
-        //modify database to change team ID
+    func assignPickToTeam(userId: String, creationTime: String, teamID:String){
+        //find and read pick
+        pickEvent=dbInterface.readPickEvent(userId: userId, creationTime: creationTime)
+        //delete pick on database
+        success=dbInterface.deletePickEvent(itemToDelete: pickEvent!)
+        //upload pick with new teamID to database
+        if success==1{
+            pickEvent?._assignedTeamID=teamID
+            dbInterface.createPickEvents(eventTime: (pickEvent?._eventTime)!, eventDate: (pickEvent?._eventDate)!, latitude: (pickEvent?._latitude)!, longitude: (pickEvent?._longitude)!, teamID: (pickEvent?._assignedTeamID)!)
+        }
     }
-    func removePickFromTeam(pickID:String,teamID:String){
-        //Modify database to change team ID
+    func removePickFromTeam(userId: String, creationTime: String, teamID:String){
+        //find and read pick
+        pickEvent=dbInterface.readPickEvent(userId: userId, creationTime: creationTime)
+        //delete pick on database
+        success=dbInterface.deletePickEvent(itemToDelete: pickEvent!)
+        //upload pick with new teamID to database
+        if success==1{
+            pickEvent?._assignedTeamID=teamID
+            dbInterface.createPickEvents(eventTime: (pickEvent?._eventTime)!, eventDate: (pickEvent?._eventDate)!, latitude: (pickEvent?._latitude)!, longitude: (pickEvent?._longitude)!, teamID: (pickEvent?._assignedTeamID)!)
+        }
     }
 }
-class volunteer: NSObject{
-    var teamID: String?
-    var pickCounter: Int?
-    var volunteerID: String?
-    var dbInterface = DatabaseInterface()
-    var pickevent: PickEvents?
-    
+class volunteer:NSObject{
+    var teamID:String?
+    var pickCounter:Int?
+    var volunteerID:String?
+    /*todo version 3
     func requestTeam(teamID:String){
         //Send notification to team leader
         //Message team leader of team
@@ -74,5 +94,6 @@ class volunteer: NSObject{
     func viewPickOnMap(pick:String?){
         //display pick from database
     }
+ */
 }
 
