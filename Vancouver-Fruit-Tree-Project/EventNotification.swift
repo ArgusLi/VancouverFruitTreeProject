b20199ec76e9e7ed2d171328012f442ed3f65e7b
Argus Li <argus@imac.hitronhub.home>
2018-07-12 19:45:37 -0700

    Completed Adding event to calendar

--- Vancouver-Fruit-Tree-Project/EventNotification.swift
+++ Vancouver-Fruit-Tree-Project/EventNotification.swift
@@ -16,6 +16,7 @@ class EventNotification: NSObject {
     
     
     func addEventToCalendar(title: String, description: String?, startDate: Date, endDate: Date, completion: ((_ success: Bool, _ error: NSError?) -> Void)? = nil) {
+    //Add event to default calendar
         
         
         eventStore.requestAccess(to: .event, completion: { (granted, error) in
@@ -39,14 +40,24 @@ class EventNotification: NSObject {
     }
     
     func removeEventFromCalendar(startDate: Date, endDate: Date, completion: ((_ success: Bool, _ error: NSError?) -> Void)? = nil){
+    //Remove event from default calendar !important! Will crash if no such event exists
         var predicate:NSPredicate?
         var events = [EKEvent]()
         var delEvent:EKEvent?
+        var calendars=[EKCalendar]()
+        calendars.append(eventStore.defaultCalendarForNewEvents!)
         eventStore.requestAccess(to: .event, completion: { (granted, error) in
             if (granted) && (error == nil) {
-                predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars:nil)
+                predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: calendars)
                 events = eventStore.events(matching: predicate!)
                 delEvent = events[0]
+                do {
+                    try eventStore.remove(delEvent!, span: .thisEvent, commit: true)
+                } catch let e as NSError {
+                    completion?(false, e)
+                    return
+                }
+                completion?(true, nil)
             }else {
                 completion?(false, error as NSError?)
             }
