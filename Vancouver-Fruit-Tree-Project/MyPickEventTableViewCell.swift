828330e867217cce1d1446e0a6d33dd2b92aafe8
Oliver Fujiki <oliver.fujiki@gmail.com>
2018-07-17 16:15:51 -0700

    Hamburger menu colour change/ List view for picks spacing fix / Creation of myPicks (Maps and List View) / Very VERY simple MyAccount Page

---
+++ Vancouver-Fruit-Tree-Project/MyPickEventTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MyPickEventTableViewCell.swift
+//  Vancouver-Fruit-Tree-Project
+//
+//  Created by Oliver Fujiki on 2018-07-16.
+//  Copyright © 2018 Harvest8. All rights reserved.
+//
+
+import UIKit
+
+class MyPickEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+    @IBOutlet weak var Date: UILabel!
+    @IBOutlet weak var Time: UILabel!
+    @IBOutlet weak var TeamLead: UILabel!
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
