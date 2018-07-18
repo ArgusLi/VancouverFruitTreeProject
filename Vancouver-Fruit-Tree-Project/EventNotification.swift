e967383c6bb9f7b725ba573edf0c5d7be5f44eb7
Argus Li <argus@imac.hitronhub.home>
2018-07-18 15:41:18 -0700

    While loop added

--- Vancouver-Fruit-Tree-Project/EventNotification.swift
+++ Vancouver-Fruit-Tree-Project/EventNotification.swift
@@ -17,39 +17,45 @@ class EventNotification: NSObject {
     
     func addEventToCalendar(title: String, description: String?, startDate: Date, endDate: Date, completion: ((_ success: Bool, _ error: NSError?) -> Void)? = nil) {
     //Add event to default calendar
+        var access = 0
         
-        
-        eventStore.requestAccess(to: .event, completion: { (granted, error) in
-            if (granted) && (error == nil) {
-                event.title = title
-                event.startDate = startDate
-                event.endDate = endDate
-                event.notes = description
-                
-                event.calendar = eventStore.defaultCalendarForNewEvents
-                do {
-                    try eventStore.save(event, span: .thisEvent, commit: true)
-                } catch let e as NSError {
-                    completion?(false, e)
-                    return
+        while access == 0{
+            eventStore.requestAccess(to: .event, completion: { (granted, error) in
+                if (granted) && (error == nil) {
+                    event.title = title
+                    event.startDate = startDate
+                    event.endDate = endDate
+                    event.notes = description
+                    access = 1
+                    event.calendar = eventStore.defaultCalendarForNewEvents
+                    do {
+                        try eventStore.save(event, span: .thisEvent, commit: true)
+                    } catch let e as NSError {
+                        completion?(false, e)
+                        return
+                    }
+                    completion?(true, nil)
+                } else {
+                    completion?(false, error as NSError?)
                 }
-                completion?(true, nil)
-            } else {
-                completion?(false, error as NSError?)
-            }
-        })
+            })
+        }
     }
     
     func findEvent(startDate: Date, endDate: Date) -> EKEvent? {
         var predicate:NSPredicate?
         var events = [EKEvent]()
-        eventStore.requestAccess(to: .event, completion: { (granted, error) in
-            if (granted) && (error == nil) {
-                predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
-            }else {
-                return
-            }
-        })
+        var access = 0
+        while access == 0 {
+            eventStore.requestAccess(to: .event, completion: { (granted, error) in
+                if (granted) && (error == nil) {
+                    predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+                    access = 1
+                }else {
+                    return
+                }
+            })
+        }
         if predicate != nil {
             if let aPredicate = predicate {
                 events = eventStore.events(matching: aPredicate)
@@ -65,21 +71,25 @@ class EventNotification: NSObject {
     //Remove event from default calendar !important! Will crash if no such event exists
         var calendars=[EKCalendar]()
         var success = 1
+        var access = 0
         calendars.append(eventStore.defaultCalendarForNewEvents!)
-        eventStore.requestAccess(to: .event, completion: { (granted, error) in
-            if (granted) && (error == nil) {
-                do {
-                    try eventStore.remove(delEvent, span: .thisEvent, commit: true)
-                } catch let e as NSError {
+        while access == 0{
+            eventStore.requestAccess(to: .event, completion: { (granted, error) in
+                if (granted) && (error == nil) {
+                    do {
+                        try eventStore.remove(delEvent, span: .thisEvent, commit: true)
+                        access = 1
+                    } catch let e as NSError {
+                        success = 0
+                        return
+                    }
+                    
+                }else {
                     success = 0
                     return
                 }
-                
-            }else {
-                success = 0
-                return
-            }
-        })
+            })
+        }
         return success
     }
 }
